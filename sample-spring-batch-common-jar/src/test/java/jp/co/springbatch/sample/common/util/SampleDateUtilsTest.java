2a0ed87dd21b8f402925762f4c7043c8b8dcb880
m.akihisa <m.akihisa@sample.com>
2019-01-21 22:26:53 +0900

    [add] src/main/resourcesをクラスパスから削除

---
+++ sample-spring-batch-common-jar/src/test/java/jp/co/springbatch/sample/common/util/SampleDateUtilsTest.java
@@ -0,0 +1,13 @@
+package jp.co.springbatch.sample.common.util;
+
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+public class SampleDateUtilsTest {
+
+  @Test
+  public void test() {
+    fail("まだ実装されていません");
+  }
+
+}
